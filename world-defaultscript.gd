f01db32517b9c00b5367fd96346fa7317bad9718
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-09-28 17:09:44 +0100

    add esc goes to title stuff

--- world-defaultscript.gd
+++ world-defaultscript.gd
@@ -4,3 +4,6 @@ func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("backspace"):
 		print("backspace pressed, resetting")
 		get_tree().reload_current_scene()
+	if Input.is_action_just_pressed("esc"):
+		print("esc pressed, going to title")
+		get_tree().change_scene_to_file("res://TitleScreen/title_screen.tscn")
