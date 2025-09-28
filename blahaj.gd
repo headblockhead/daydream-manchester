f9166e8a40dec3866b545d1122611dc1d254d203
Edward Hesketh <inbox@edwardh.dev>
2025-09-28 17:44:09 +0100

    fix: jumpy

--- blahaj.gd
+++ blahaj.gd
@@ -6,13 +6,15 @@ extends CharacterBody2D
 @export var FRICTION: int = 400
 const JUMP_VELOCITY = -400.0
 
+const JUMP_THRESHOLD : float = 100
+
 @export var nav_agent: NavigationAgent2D
 @export var body: AnimatedSprite2D;
 
 func _ready() -> void:
-	nav_agent.path_desired_distance = 200.0
-	nav_agent.target_desired_distance = 50.0
-	nav_agent.path_max_distance = 200.0
+	nav_agent.path_desired_distance = 300.0
+	nav_agent.target_desired_distance = sqrt(2*(32^2))
+	nav_agent.path_max_distance = 500.0
 	
 func _physics_process(delta: float) -> void:
 	var direction: Vector2 = nav_agent.get_next_path_position()-global_position
@@ -24,7 +26,6 @@ func _physics_process(delta: float) -> void:
 			velocity = velocity.move_toward(direction * SPEED, ACCELERATION * delta )
 		else:
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
-
 		move_and_slide()
 
 	if not is_on_floor():
@@ -41,5 +42,7 @@ func change_direction(direction: float) -> void:
 		body.flip_h = true	
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept"):
+		velocity.y = JUMP_VELOCITY
+	elif is_on_floor() && velocity.x == 0 && (position.x + JUMP_THRESHOLD < direction || position.x - JUMP_THRESHOLD > direction):
 		velocity.y = JUMP_VELOCITY
